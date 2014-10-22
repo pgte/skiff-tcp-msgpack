897aedf7598f105ae777bee5f8cf172201795a20
Pedro Teixeira <i@pgte.me>
2014-10-22 15:50:15 +0100

    switched to msgpack-stream

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var net = require('net');
-var msgpack = require('msgpack5')();
+var msgpack = require('msgpack-stream');
 var test = require('abstract-skiff-transport/test/all');
 var Transport = require('./');
 
@@ -16,36 +16,26 @@ var options = {
 
     function onConnection(c) {
 
-      c.on('data', function(d) {
-        console.log('mock server got data:', d);
-      });
-
       var oldWrite = c.write;
       c.write = function (o) {
-        console.log('writing', o);
         oldWrite.apply(c, arguments);
       };
 
       server.__connections.push(c);
 
-      var encoder = msgpack.encoder();
-      var decoder = msgpack.decoder();
+      var encoder = msgpack.createEncodeStream();
+      var decoder = msgpack.createDecodeStream();
       c.__encoder = encoder;
       c.__decoder = decoder;
-
-      encoder.on('data', function(d) {
-        console.log('ENCOER DATA:', d);
-      });
 
       encoder.pipe(c).pipe(decoder);
 
       decoder.on('data', onMessage);
 
       function onMessage(m) {
-        console.log('gotz message:', m);
         if (m.request) {
           m.request.unshift(undefined);
-          encoder.write({response: d.request});
+          encoder.write({response: m.request});
         }
       }
 
@@ -68,11 +58,10 @@ var options = {
   },
   intercept: function(server, cb) {
     server.__connections.forEach(function(c) {
-      c__decoder.on('data', onMessage);
+      c.__decoder.on('data', onMessage);
     });
 
     function onMessage(m) {
-      var d = JSON.parse(l);
       cb(m.response);
     }
   },
@@ -80,8 +69,8 @@ var options = {
     var self = this;
 
     var c = net.connect(8081);
-    var encoder = msgpack.encoder();
-    var decoder = msgpack.decoder();
+    var encoder = msgpack.createEncodeStream();
+    var decoder = msgpack.createDecodeStream();
     encoder.pipe(c).pipe(decoder);
     encoder.write({hello: self.listenPeerId});
     decoder.on('data', function() {});
